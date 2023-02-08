6baf9efe5e6bdf71bd2b8993d58c5585de92da67
moreDasha <moredasha@icloud.com>
2023-02-08 21:14:22 +0400

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let nString = n < 0 ? (n * (-1)).toString() : n.toString();
+    let result = '';
+    let i = nString.length - 1;
+    while (i >= 0) {
+      result = result + nString[i];
+      i = i - 1;
+    }
+    return Number(result)
 }
